6994ac2a0d1380ccc15c612964b13841ba714a15
Yaniv Aran-Shamir <yaniv.aran-shamir@cloudinary.com>
2022-03-17 09:41:10 +0200

    fix removing false entries in upload

--- api/uploader/upload_test.go
+++ api/uploader/upload_test.go
@@ -2,6 +2,7 @@ package uploader_test
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -251,6 +252,25 @@ func TestUploader_UploadWithResponsiveBreakpoints(t *testing.T) {
 	assert.Equal(t, "a_90", eResp.ResponsiveBreakpoints[0].Transformation)
 	assert.Equal(t, "a_45", eResp.ResponsiveBreakpoints[1].Transformation)
 }
+
+func TestUploadParmasBoolean(t *testing.T) {
+	params := uploader.UploadParams{
+		UniqueFilename: false,
+		Overwrite: false,
+	}
+	paramsBytes, err := json.Marshal(params)
+	if err != nil {
+		t.Error("Error marshalling %w", err)
+	}
+	p := uploader.UploadParams{}
+	err = json.Unmarshal(paramsBytes, &p)
+	if err != nil {
+		t.Error("Error unmarshalling %w", err)
+	}
+	assert.Equal(t, false, p.Overwrite)
+	assert.Equal(t, false, p.UniqueFilename)
+}
+
 
 func populateLargeImage() string {
 	head := "BMJ\xB9Y\x00\x00\x00\x00\x00\x8A\x00\x00\x00|\x00\x00\x00x\x05\x00\x00x\x05\x00\x00\x01\x00\x18\x00" +
